4acf013cc25b1adc6ebe354429a5b99a934e5fda
Ben Borszcz <ben.borszcz@gmail.com>
2023-11-24 12:16:12 -0500

    works for outer rings

--- archery_scorer/target_detector.py
+++ archery_scorer/target_detector.py
@@ -16,16 +16,48 @@ class TargetDetector:
         """
         Detect the corners of the target face.
         """
+        # Assuming the target face is the largest square-like contour
+        # This method would be implemented if needed
         pass
     
     def rectify_target(self):
         """
         Rectify the image so that the target face is frontal and circular.
         """
+        # Assuming the preprocessor already rectifies the image to a circle
+        # This method would be implemented if needed
         pass
     
-    def detect_circles(self):
+    def detect_circles(self, dp=1.2, minDist=0.1, param1=150, param2=450, minRadius=10, maxRadius=2000):
         """
-        Detect the concentric circles of the target face.
+        Detect the concentric circles of the target face using Hough Circle Transform.
+        :param dp: Inverse ratio of the accumulator resolution to the image resolution.
+        :param minDist: Minimum distance between the centers of the detected circles.
+        :param param1: Higher threshold for the Canny edge detector.
+        :param param2: Threshold for center detection.
+        :param minRadius: Minimum circle radius.
+        :param maxRadius: Maximum circle radius.
+        :return: A list of detected circles with (x, y, radius).
         """
-        pass
+        #gray_image = cv2.cvtColor(self.preprocessed_image, cv2.COLOR_BGR2GRAY)
+        circles = cv2.HoughCircles(
+            self.preprocessed_image,
+            cv2.HOUGH_GRADIENT,
+            dp=dp,
+            minDist=minDist,
+            param1=param1,
+            param2=param2,
+            minRadius=minRadius,
+            maxRadius=maxRadius
+        )
+        
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("int")
+            return circles
+        else:
+            return []
+
+# Example usage:
+# preprocessed_image = ... # This should be the output from the preprocessor
+# target_detector = TargetDetector(preprocessed_image)
+# circles = target_detector.detect_circles()
---
+++ tests/test_target_detector.py
@@ -0,0 +1,61 @@
+import unittest
+import cv2
+import numpy as np
+import sys
+import os
+
+# Adjust the path to import the necessary modules
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from archery_scorer.target_detector import TargetDetector
+from archery_scorer.preprocessor import ImagePreprocessor
+
+class TestTargetDetector(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # This method will be executed once before any test is run
+        # Load a test image or create a synthetic one for testing purposes
+        cls.test_image_path = 'tests/data/8-09.jpg'  # Provide a valid path to a test image
+        cls.preprocessor = ImagePreprocessor(cls.test_image_path)
+        cls.preprocessed_image = cls.preprocessor.detect_and_correct_ovals()
+        
+
+    def show_image(self, image, title="Image", wait_key_time=0, scale=0.75):
+        """
+        Display the image in a window with optional scaling.
+        :param image: The image to be displayed.
+        :param title: The title of the window.
+        :param wait_key_time: Time in milliseconds to wait for a key event. 0 means wait indefinitely.
+        :param scale: Scaling factor for the image size.
+        """
+        if scale != 1.0:
+            # Calculate the new dimensions based on the scaling factor
+            new_width = int(image.shape[1] * scale)
+            new_height = int(image.shape[0] * scale)
+            # Resize the image using the new dimensions
+            scaled_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
+        else:
+            scaled_image = image
+
+        cv2.imshow(title, scaled_image)
+        cv2.waitKey(wait_key_time)
+        cv2.destroyAllWindows()
+
+    def test_detect_circles(self):
+        # Initialize the TargetDetector with the preprocessed image
+        target_detector = TargetDetector(self.preprocessed_image)
+        # Detect circles on the target face
+        circles = target_detector.detect_circles()
+        # Assert that circles have been detected
+        self.assertIsNotNone(circles, "Circles should be detected")
+        self.assertGreater(len(circles), 0, "At least one circle should be detected")
+
+        # Optionally, visualize the detected circles on the image
+        for circle in circles:
+            cv2.circle(self.preprocessed_image, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
+        self.show_image(self.preprocessed_image, title="Detected Circles", wait_key_time=0)
+
+    # Additional test cases can be added here to test other functionalities
+    # such as find_target_corners, rectify_target, etc.
+
+if __name__ == '__main__':
+    unittest.main()
