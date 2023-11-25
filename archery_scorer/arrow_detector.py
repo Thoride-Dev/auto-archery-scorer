f02c5da594dbf27d7b13db506d5c685087dbd6c6
Kush Dalal <kushdalal007@gmail.com>
2023-11-24 22:55:31 -0500

    Preliminary arrow detection code

--- archery_scorer/arrow_detector.py
+++ archery_scorer/arrow_detector.py
@@ -17,10 +17,35 @@ class ArrowDetector:
         """
         Detect gaps in the concentric circles that may indicate the presence of arrows.
         """
-        pass
+        gaps = []
+        if len(self.circles) > 1:
+            for i in range(len(self.circles) - 1):
+                # Calculate the distance between circles
+                distance = np.abs(self.circles[i][0][2] - self.circles[i + 1][0][2])
+                gaps.append(distance)
+        return gaps
     
-    def detect_arrows(self):
+    def detect_arrow(self):
         """
-        Detect the arrows by finding lines and clusters within the target face.
+        Detect a single arrow by finding lines within the target face.
         """
-        pass
+        # Edge detection using Canny
+        edges = cv2.Canny(self.rectified_image, 50, 150)
+        
+        # Detect lines using HoughLinesP
+        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=50, maxLineGap=10)
+        
+        # Extracting line coordinates
+        line_coords = []
+        if lines is not None:
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                line_coords.append([x1, y1, x2, y2])
+        
+        # Fit a line to the arrow
+        if len(line_coords) > 0:
+            # Assuming the arrow will be the longest line detected
+            longest_line = max(line_coords, key=lambda line: np.linalg.norm(np.array(line[:2]) - np.array(line[2:])))
+            return longest_line
+        else:
+            return None
